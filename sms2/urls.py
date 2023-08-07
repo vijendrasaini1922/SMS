9bdded0c596576a005d7312e26825c8d3d060534
vijendra saini <virajsaini0@gmail.com>
2023-08-08 02:06:46 +0530

    Added files leave, feedback, view_attendance, one error facing during the new subject add need to remove it

--- sms2/urls.py
+++ sms2/urls.py
@@ -5,20 +5,58 @@ from django.conf.urls.static import static
 from sms_app import views, views_hod
 
 urlpatterns = [
+    ############# Admin Login / Logout ###########
     path('admin/', admin.site.urls),
     path('', views.loginPage),
     path('do_login', views.doLogin),
     path('get_user_details', views.getUserDetails),
     path('user_logout/', views.userLogout),
+    
+    
+    ############### Admin HOME ##################
     path('admin_home', views_hod.adminHome),
+    
+    
+    ################# Staff ######################
     path('add_staff', views_hod.addStaff),
     path('add_staff_save', views_hod.addStaffSave),
-    path('add_course', views_hod.addCourse),
-    path('add_course_save', views_hod.addCourseSave),
+    path('manage_staff/', views_hod.manageStaff),
+    
+    
+    ################# Student ####################
     path('add_student', views_hod.addStudent),
     path('add_student_save', views_hod.addStudentSave),
+    path('manage_student', views_hod.manageStudent),
+    
+    
+    ################# Course #####################
+    path('add_course', views_hod.addCourse),
+    path('add_course_save', views_hod.addCourseSave),
+    path('manage_course', views_hod.manageCourse),
+    
+    
+    ################# Subject ####################
+    path('add_subject', views_hod.addSubject),
+    path('add_subject_save', views_hod.addSubjectSave),
+    path('manage_subject', views_hod.manageSubject),
+    
+    
+    ############### View Attendance ###############
+    path('view_attendance', views_hod.viewAttendance),
+
+    
+    ################# Feedback ####################
+    path('feedback_student', views_hod.feedbackStudent),
+    path('feedback_staff', views_hod.feedbackStaff),
+    
+    
+    ################### Leave ######################
+    path('leave_student', views_hod.leaveStudent),
+    path('leave_staff', views_hod.leaveStaff),
     
 
+
+    ################ Test Cases ####################
     # path('demo/', views.demo),   
 ]
 
--- sms_app/views_hod.py
+++ sms_app/views_hod.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
-from sms_app.models import CustomUser, Staffs, Courses, Students
+from sms_app.models import CustomUser, Staffs, Courses, Students, Subjects
 
 
 ########################### Python Module Import ######################
@@ -55,6 +55,73 @@ def addStaffSave(request):
             return HttpResponseRedirect("/add_staff")
         
         
+def manageStaff(request):
+    staffs = Staffs.objects.all()
+    return render(request, "hod_templates/manage_staff_template.html", {"staffs" : staffs})
+        
+
+##################### Add Student & Save ##########################
+#####################################################################
+
+def addStudent(request):
+    courses = Courses.objects.all()
+    # for i in courses:
+        # print(i)
+    return render(request, "hod_templates/add_student_template.html", {"courses": courses})
+
+
+def addStudentSave(request):
+    if request.method != "POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        data = request.POST
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        username = data.get("username")
+        course_id = data.get("course_name")
+        gender = data.get("gender")
+        email = data.get("email")
+        password = data.get("password")
+        address = data.get("address")
+        session_start_year = data.get("session_start_year")
+        session_end_year = data.get("session_end_year")
+        
+        ########### Add Student Email, Password, username, first_name, last_name : for Login#######
+        try:
+            user = CustomUser.objects.create_user(username = username, 
+                                                  email = email, 
+                                                  password = password, 
+                                                  first_name = first_name, 
+                                                  last_name = last_name, 
+                                                  user_type = 3,
+                                                  )
+            # Getting Course Id from Course table
+            # print(Courses.objects.get(id = course_id))
+            user.students.course_id = Courses.objects.get(id = course_id)
+            user.students.address = address
+            user.students.gender = gender
+            # print(sex)
+            user.students.session_start_year = session_start_year
+            # print(session_start_year)
+            # print(user.students.session_start_year)
+            user.students.session_end_year = session_end_year
+            
+            # print(user)
+            user.save()
+            messages.success(request, "Successfully Added")
+            return HttpResponseRedirect("/add_student")
+        
+        except Exception as e:
+            # print(e)
+            messages.error(request, f"Failed To Add !! Because ({e})")
+            return HttpResponseRedirect("/add_student")
+        
+
+def manageStudent(request):
+    students = Students.objects.all()
+    return render(request, "hod_templates/manage_student_template.html", {"students" : students})
+
+
 ##################### Create Course  ################################
 #####################################################################
 
@@ -81,52 +148,94 @@ def addCourseSave(request):
             return HttpResponseRedirect("/add_course")
     
 
-##################### Add Student & Save ##########################
+def manageCourse(request):
+    courses = Courses.objects.all()
+    return render(request, "hod_templates/manage_course_template.html", {"courses" : courses})
+
+
+##################### Create Subject  ################################
 #####################################################################
 
-def addStudent(request):
+def addSubject(request):
     courses = Courses.objects.all()
-    return render(request, "hod_templates/add_student_template.html", {"courses": courses})
+    staffs = CustomUser.objects.filter(user_type=2)
+    for i in staffs:
+        print(i.first_name, i.last_name, i.id)
+    return render(request, "hod_templates/add_subject_template.html",
+                  {"courses" : courses, "staffs": staffs})
 
 
-def addStudentSave(request):
+def addSubjectSave(request):
     if request.method != "POST":
         return HttpResponse("Method Not Allowed")
+    
     else:
         data = request.POST
-        first_name = data.get("first_name")
-        last_name = data.get("last_name")
-        username = data.get("username")
-        course_id = data.get("course_name")
-        sex = data.get("sex")
-        email = data.get("email")
-        password = data.get("password")
-        address = data.get("address")
-        session_start_year = data.get("session_start_year")
-        session_end_year = data.get("session_end_year")
+        subject_name = data.get("subject_name")
+        # print(subject_name)
+        ## Here we get id when we fetch
+        course_id = data.get("course_id")
+        staff_id = data.get("staff_id")
+        print(course_id)
+        print(staff_id)
         
-        ########### Add Student Email, Password, username, first_name, last_name : for Login#######
         try:
-            user = CustomUser.objects.create_user(username = username, 
-                                                  email = email, 
-                                                  password = password, 
-                                                  first_name = first_name, 
-                                                  last_name = last_name, 
-                                                  user_type = 3,
-                                                  )
-            # Getting Course Id from Course table
-            user.students.course_id = Courses.objects.get(id = course_id)
-            user.students.address = address
-            user.students.sex = sex
-            user.students.session_start_year = session_start_year
-            user.students.session_end_year = session_end_year
+            ## Course id, staff id instance creating from Courses, Staffs class
+            course_instance = Courses.objects.get(id=course_id)
+            staff_instance = CustomUser.objects.get(id=staff_id)
+            print(course_instance)
+            print(staff_instance)
+                
+        except (Courses.DoesNotExist, Staffs.DoesNotExist):
+            messages.error(request, "Course or Staff Doesn't Exist")
+            return HttpResponseRedirect("/add_subject")
             
-            print(user)
-            user.save()
+        try:
+            subject = Subjects(subject_name=subject_name,
+                               course_id=course_instance,
+                               staff_id=staff_instance)
+            
+            subject.save()
             messages.success(request, "Successfully Added")
-            return HttpResponseRedirect("/add_student")
-        
+            return HttpResponseRedirect("/add_subject")
+            
         except Exception as e:
-            print(e)
-            messages.error(request, f"Failed To Add !! Because ({e})")
-            return HttpResponseRedirect("/add_student")
+            messages.error(request, f"Failed due to error : ({e})")
+            return HttpResponseRedirect("/add_subject")
+              
+
+def manageSubject(request):
+    subjects = Subjects.objects.all()
+    return render(request, "hod_templates/manage_subject_template.html", {"subjects": subjects})
+
+
+##################### View Attendance ################################
+######################################################################
+
+def viewAttendance(request):
+    #TODO
+    return render(request, "hod_templates/view_attendance_template.html")
+
+
+##################### Feedback ######################################
+#####################################################################
+
+def feedbackStudent(request):
+    #TODO
+    return render(request, "hod_templates/feedback_student_template.html")
+
+def feedbackStaff(request):
+    #TODO
+    return render(request, "hod_templates/feedback_staff_template.html")
+
+
+##################### Leave #########################################
+#####################################################################
+
+def leaveStudent(request):
+    #TODO
+    return render(request, "hod_templates/leave_student_template.html")
+
+def leaveStaff(request):
+    #TODO
+    return render(request, "hod_templates/leave_staff_template.html")
